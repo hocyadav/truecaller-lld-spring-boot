1e12c5fbcb1428adc210296d7b2020c46d065b87
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-14 16:51:45 +0530

    dao added

---
+++ src/main/java/io/hari/demo/dao/UserDao.java
@@ -0,0 +1,12 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.User;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 14-03-2021
+ */
+@Repository
+public interface UserDao extends BaseDao<User>{
+}
