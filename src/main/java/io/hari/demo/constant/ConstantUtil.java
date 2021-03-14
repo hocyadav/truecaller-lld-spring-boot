660eac4d74f538df75d8c43ec6344703957a0cc2
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-14 16:52:21 +0530

    constant added

---
+++ src/main/java/io/hari/demo/constant/ConstantUtil.java
@@ -0,0 +1,12 @@
+package io.hari.demo.constant;
+
+/**
+ * @Author Hariom Yadav
+ * @create 14-03-2021
+ */
+public class ConstantUtil {
+    public static final String BLOCKED_CONTACT_CALLING = "blocked contact calling... ";
+    public static final String SAVED_CONTACT_CALLING = "saved contact calling... ";
+    public static final String GLOBAL_DIRECTORY_CONTACT_CALLING = "global directory contact calling... ";
+    public static final String NEW_NUMBER_CALLING = "new number calling... ";
+}
